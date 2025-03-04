6c38b36e7691b69583a09be22b5abc7eca0f3d9c
ali.rashad <ali.rashad@cafu.com>
2025-03-04 20:43:34 +0200

    implement viewmodel - implement unit tests

---
+++ pdp-ui/src/main/java/com/banquemisr/pdpui/presentation/viewmodel/PdpViewModel.kt
@@ -0,0 +1,45 @@
+package com.banquemisr.pdpui.presentation.viewmodel
+
+import androidx.lifecycle.*
+import com.banquemisr.pdpcomponent.domain.model.MovieDetails
+import com.banquemisr.pdpcomponent.domain.usecase.FetchMovieDetails
+import com.banquemisr.viewmodel.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+internal class PdpViewModel @Inject constructor(
+    private val fetchMovieDetails: FetchMovieDetails,
+    private val stateDelegate: StateDelegate<State> = StateDelegate()
+) : ViewModel(),
+    DefaultLifecycleObserver,
+    StateViewModel<PdpViewModel.State> by stateDelegate {
+
+    init {
+        stateDelegate.setDefaultState(State.Idle)
+    }
+
+    fun fetchMovieDetails(id: String) {
+        stateDelegate.updateState { State.Loading }
+        viewModelScope.launch {
+            fetchMovieDetails.invoke(id).fold(
+                success = { movie ->
+                    stateDelegate.updateState { State.Content(movie) }
+                },
+                error = {
+                    stateDelegate.updateState { State.Error }
+                }
+            )
+        }
+    }
+
+    sealed interface State {
+        data object Idle : State
+        data object Loading : State
+        data object Error : State
+        data class Content(
+            val movie: MovieDetails
+        ) : State
+    }
+}
