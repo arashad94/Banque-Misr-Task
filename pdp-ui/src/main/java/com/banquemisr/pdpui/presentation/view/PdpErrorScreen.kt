50728d4b1458a8ea174f86d4d180504905e6c1b1
ali.rashad <ali.rashad@cafu.com>
2025-03-06 04:37:21 +0200

    add lokalise hint

--- pdp-ui/src/main/java/com/banquemisr/pdpui/presentation/view/PdpErrorScreen.kt
+++ pdp-ui/src/main/java/com/banquemisr/pdpui/presentation/view/PdpErrorScreen.kt
@@ -3,8 +3,10 @@ package com.banquemisr.pdpui.presentation.view
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
+import androidx.compose.ui.res.stringResource
 import com.banquemisr.designsystem.BMText.HeadingL
 import com.banquemisr.designsystem.BMTextModifier
+import com.banquemisr.pdpui.R
 
 @Composable
 internal fun PdpErrorScreen() {
@@ -13,7 +15,7 @@ internal fun PdpErrorScreen() {
             textModifier = BMTextModifier(
                 modifier = Modifier.align(Alignment.Center)
             ),
-            text = "Something went wrong"
+            text = stringResource(R.string.something_went_wrong) // I prefer using Lokalise to enhance as it delegates the responsibility of localization to the content team
         )
     }
 }
